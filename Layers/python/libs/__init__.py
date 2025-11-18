aa9d3c7654f56c7ed34e3f01dbf06a43aea7a081
Josue Hernandez Yataco <josue.hernandez@utec.edu.pe>
2025-11-18 20:45:09 +0000

    refactor: mejorar estructura y limpieza de dependencias en el script de despliegue

---
+++ Layers/python/libs/__init__.py
@@ -0,0 +1,4 @@
+"""
+Dependencias de terceros instaladas con pip
+Este directorio contiene las bibliotecas externas requeridas por los microservicios
+"""
--- Layers/python/utils/__init__.py
+++ Layers/python/utils/__init__.py
@@ -1,3 +1,21 @@
 """
-Utils compartidos para todas las Lambdas de ChinaWok
+Utilidades compartidas para todos los microservicios de ChinaWok
 """
+
+# Importar los principales módulos para facilitar su uso
+from .logger import get_logger
+from .json_encoder import json_dumps, DecimalEncoder
+from .dynamodb_client import get_dynamodb_resource, get_table_data
+from .s3_client import upload_to_s3, download_from_s3
+from .athena_client import AthenaQueryExecutor
+
+__all__ = [
+    'get_logger',
+    'json_dumps',
+    'DecimalEncoder',
+    'get_dynamodb_resource',
+    'get_table_data',
+    'upload_to_s3',
+    'download_from_s3',
+    'AthenaQueryExecutor',
+]
